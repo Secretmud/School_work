340c3959fffec3bbcc8fc9787885532dcf53ce52
Secretmud <lamefeed@gmail.com>
2019-02-26 11:31:59 +0100

    Update and rename Main.c to bubble_sort.c
    Name is changed to sort name

--- Sorts/bubble_sort.c
+++ Sorts/bubble_sort.c
@@ -8,7 +8,7 @@ int main()
 	printf("|SoA\t|AP\t|AV\t\t|Value\t|Hold\t|\n");
 	printf("Pre sort\n");
 	for (int x = 0; x < 4; x++)
-		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr) / sizeof(int), x, x, arr[x]);
+		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr)/sizeof(int), x, x, arr[x]);
 	printf("Sort\n");
 	for (int x = 0; x < 5; x++)
 	{
@@ -24,5 +24,5 @@ int main()
 	}
 	printf("Post sort\n");
 	for (int x = 0; x < 4; x++)
-		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr) / sizeof(int), x, x, arr[x]);
+		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr)/sizeof(int), x, x, arr[x]);
 }
