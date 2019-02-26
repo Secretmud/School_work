199a7fbcfb29a4c1a7503f6ea9f10269a0c95bd8
Secretmud <lamefeed@gmail.com>
2019-02-26 10:34:55 +0100

    Create Main.c
    Learning about sorting. Adding my first bubble sort. Might change it.

---
+++ Sorts/Main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int arr[8] = { 9, 8, 4, 2};
+	int holder;
+	printf("|SoA\t|AP\t|AV\t\t|Value\t|Hold\t|\n");
+	printf("Pre sort\n");
+	for (int x = 0; x < 4; x++)
+		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr) / sizeof(int), x, x, arr[x]);
+	printf("Sort\n");
+	for (int x = 0; x < 5; x++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (arr[i] > arr[i + 1]) {
+				holder = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = holder;
+				printf("| %d \t| %d \t| arr[%d] \t| %d \t| %d\t|\n", sizeof(arr)/sizeof(int), i, i, arr[i], holder);
+			}
+		}
+	}
+	printf("Post sort\n");
+	for (int x = 0; x < 4; x++)
+		printf("| %d \t| %d \t| arr[%d] \t| %d \t|\t|\n", sizeof(arr) / sizeof(int), x, x, arr[x]);
+}
