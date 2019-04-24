6764b4f87d1a099b3011a349ea3638e87f4b75f0
secretmud <lamefeed@gmail.com>
2019-04-24 14:05:55 +0200

    Added menu_fields(), menu_switch(), game(), game_print(). You can now play different 'games'. The info() statement is also printing height without the offset and the offset increment

--- final_project_ELI1500/main/main.ino
+++ final_project_ELI1500/main/main.ino
@@ -1,5 +1,51 @@
+/*
+    _,.---._      ,-,--.             _,.---._           ___      ,----.  ,--.--------.
+  ,-.' , -  `.  ,-.'-  _\  _.-.    ,-.' , -  `.  .-._ .'=.'\  ,-.--` , \/==/,  -   , -\
+ /==/_,  ,  - \/==/_ ,_.'.-,.'|   /==/_,  ,  - \/==/ \|==|  ||==|-  _.-`\==\.-.  - ,-./
+|==|   .=.     \==\  \  |==|, |  |==|   .=.     |==|,|  / - ||==|   `.-. `--`\==\- \
+|==|_ : ;=:  - |\==\ -\ |==|- |  |==|_ : ;=:  - |==|  \/  , /==/_ ,    /      \==\_ \
+|==| , '='     |_\==\ ,\|==|, |  |==| , '='     |==|- ,   _ |==|    .-'       |==|- |
+ \==\ -    ,_ //==/\/ _ |==|- `-._\==\ -    ,_ /|==| _ /\   |==|_  ,`-._      |==|, |
+  '.='. -   .' \==\ - , /==/ - , ,/'.='. -   .' /==/  / / , /==/ ,     /      /==/ -/
+    `--`--''    `--`---'`--`-----'   `--`--''   `--`./  `--``--`-----``       `--`--`
+
+   ,-,--.  ,--.--------.   _,.---._
+ ,-.'-  _\/==/,  -   , -\,-.' , -  `.   .-.,.---.     _..---.  ,--.-.  .-,--.
+/==/_ ,_.'\==\.-.  - ,-./==/_,  ,  - \ /==/  `   \  .' .'.-. \/==/- / /=/_ /
+\==\  \    `--`\==\- \ |==|   .=.     |==|-, .=., |/==/- '=' /\==\, \/=/. /
+ \==\ -\        \==\_ \|==|_ : ;=:  - |==|   '='  /|==|-,   '  \==\  \/ -/
+ _\==\ ,\       |==|- ||==| , '='     |==|- ,   .' |==|  .=. \  |==|  ,_/
+/==/\/ _ |      |==|, | \==\ -    ,_ /|==|_  . ,'. /==/- '=' ,| \==\-, /
+\==\ - , /      /==/ -/  '.='. -   .' /==/  /\ ,  )==|   -   /  /==/._/
+ `--`---'       `--`--`    `--`--''   `--`-`--`--'`-._`.___,'   `--`-`
+
+              .-._         .=-.-.       ,-.-.    ,----.                ,-,--.   .=-.-.
+ .--.-. .-.-./==/ \  .-._ /==/_ /,--.-./=/ ,/ ,-.--` , \  .-.,.---.  ,-.'-  _\ /==/_ /
+/==/ -|/=/  ||==|, \/ /, /==|, |/==/, ||=| -||==|-  _.-` /==/  `   \/==/_ ,_.'|==|, |,--.--------.
+|==| ,||=| -||==|-  \|  ||==|  |\==\,  \ / ,||==|   `.-.|==|-, .=., \==\  \   |==|  /==/,  -   , -\
+|==|- | =/  ||==| ,  | -||==|- | \==\ - ' - /==/_ ,    /|==|   '='  /\==\ -\  |==|- \==\.-.  - ,-./
+|==|,  \/ - ||==| -   _ ||==| ,|  \==\ ,   ||==|    .-' |==|- ,   .' _\==\ ,\ |==| ,|`--`--------`
+|==|-   ,   /|==|  /\ , ||==|- |  |==| -  ,/|==|_  ,`-._|==|_  . ,'./==/\/ _ ||==|- |
+/==/ , _  .' /==/, | |- |/==/. /  \==\  _ / /==/ ,     //==/  /\ ,  )==\ - , //==/. /
+`--`..---'   `--`./  `--``--`-`    `--`--'  `--`-----`` `--`-`--`--' `--`---' `--`-`
+ ,--.--------.    ,----.  ,--.--------.    ,----.  ,--.--------.
+/==/,  -   , -\,-.--` , \/==/,  -   , -\,-.--` , \/==/,  -   , -\
+\==\.-.  - ,-./==|-  _.-`\==\.-.  - ,-./==|-  _.-`\==\.-.  - ,-./
+ `--`\==\- \  |==|   `.-. `--`\==\- \  |==|   `.-. `--`\==\- \
+      \==\_ \/==/_ ,    /      \==\_ \/==/_ ,    /      \==\_ \
+      |==|- ||==|    .-'       |==|- ||==|    .-'       |==|- |
+      |==|, ||==|_  ,`-._      |==|, ||==|_  ,`-._      |==|, |
+      /==/ -//==/ ,     /      /==/ -//==/ ,     /      /==/ -/
+      `--`--``--`-----``       `--`--``--`-----``       `--`--`
+
+Gruppe 7:
+    Oppgave:
+        Vanndispenser for dyr(med litt forandring)
+
+*/
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#define menu_len 16
 //Pin declaration
 int trigger = 9;
 int echo = 10;
@@ -9,6 +55,7 @@ int clk = 4;
 //Variables needed for the program to run
 int height = 0;
 int offset = 3;
+bool set = false; //This is used to determine the type of game
 //Used by the ultrasonic sensor
 bool calibrated = false;
 long distance;
@@ -18,11 +65,22 @@ int counter = 0;
 int dtState;
 int dtLastState;
 //Menu arrays
-char menu[][16] = {{"Calibration"}, {"Drink!!!"}, {"Info"}};
+char menu[][menu_len] = {
+                         {"Calibration"},
+                         {"Drink menu"},
+                         {"System info"}
+                        };
+char menu_sub[][16] = {
+                       {"Mix: 10%"}, {"Mix: 20%"},
+                       {"Mix: 30%"}, {"Random drink"},
+                       {"back"}
+                      };
 char name[] = {"Drink'o'matic!"};
 //function prototypes
 int calibration(int offset);
 void menu_fields(int x);
+void menu_switch(int x);
+void menu_sub_switch(void);
 void game(int height);
 void info(int height, int offset);
 //Initializing the 16x2 LCD with a I2C connector
@@ -52,6 +110,7 @@ void setup()
 
 void loop()
 {
+    int menu_size = sizeof(menu)/menu_len -1;
     if (!calibrated) {
         lcd.clear();
         height = calibration(offset);
@@ -71,44 +130,91 @@ void loop()
             }
             if ( counter < 0)
                 counter = 0;
-            if ( counter > 2)
-                counter = 2;
+            if ( counter > menu_size)
+                counter = menu_size;
             Serial.print("Position: ");
             Serial.println(counter);
             menu_fields(counter);
         }
         if (digitalRead(btn) == LOW) {
             Serial.println(counter);
-            switch(counter) {
-                case 0:
-                    if (calibrated) {
-                        lcd.clear();
-                        height = calibration(offset);
-                        lcd.setCursor(0,0);
-                        lcd.print("Height:");
-                        lcd.setCursor(1,1);
-                        lcd.print(height);
-                        lcd.setCursor(1+sizeof(height),1);
-                        lcd.print("cm");
-                    }
-                    break;
-                case 1:
-                    Serial.println("This");
-                    lcd.clear();
-                    lcd.print("The game");
-                    break;
-                case 2:
-                    info(height, offset);
-                    break;
-            }
+            menu_switch(counter);
         }       
         dtLastState = dtState;
     }
 }
 
+void menu_switch(int counter)
+{
+    switch(counter) {
+        case 0:
+            if (calibrated) {
+                lcd.clear();
+                height = calibration(offset);
+                lcd.setCursor(0,0);
+                lcd.print("Height:");
+                lcd.setCursor(1,1);
+                lcd.print(height);
+                lcd.setCursor(1+sizeof(height),1);
+                lcd.print("cm");
+            }
+            break;
+        case 1:
+            menu_sub_switch();
+        case 2:
+            info(height, offset);
+            break;
+        default:
+            break;
+    }
+
+}
+
+void menu_sub_switch()
+{
+    int menu_size = sizeof(menu_sub)/menu_len -1;
+    dtState = digitalRead(dt);
+    if (dtState != dtLastState) {
+        if (digitalRead(clk) != dtState) {
+            counter++;
+        } else {
+            counter--;
+        }
+        if ( counter < 0)
+            counter = 0;
+        if ( counter > menu_size)
+            counter = menu_size;
+        Serial.print("Position: ");
+        Serial.println(counter);
+        switch(counter)
+        {
+        case 0:
+            set = false;
+            game(height, set, 0);
+            break;
+        case 1:
+            set = true;
+            game(height, set, 10);
+            break;
+        case 2:
+            set = true;
+            game(height, set, 20);
+            break;
+        case 3:
+            set = true;
+            game(height, set, 30);
+            break;
+        }
+    }
+    if (digitalRead(btn) == LOW) {
+        Serial.println(counter);
+        menu_switch(counter);
+    }       
+    dtLastState = dtState;
+}
+
 int calibration(int offset)
 {
-    int height = 0;
     digitalWrite(trigger, LOW);
     delayMicroseconds(2);
     digitalWrite(trigger, HIGH);
@@ -122,19 +228,39 @@ int calibration(int offset)
     return distance + offset;
 }
 
-void menu_fields(int x)
+void menu_fields(int current)
 {
     lcd.clear();
     lcd.print(">");
     lcd.setCursor(1,0);
-    lcd.print(menu[x]);
-    lcd.setCursor(0,1);
-    lcd.print(menu[x+1]);
+    lcd.print(menu[current]);
+    lcd.setCursor(1,1);
+    lcd.print(menu[current+1]);
 }
 
-void game(int height)
+void game(int height, bool set, int percentage)
 {
+    if (set) {
+        int p_x = percentage;
+        int x = (p_x/100)*height;
+        game_print(x);
+    } else {
+        int p_x = rand() % 100;
+        int x = (p_x/100)*height;
+        game_print(x);
+    }
+}
 
+void game_print(int x)
+{
+    lcd.clear();
+    lcd.print("height cm:");
+    lcd.setCursor(13,0);
+    lcd.print(height);
+    lcd.setCursor(0,1);
+    lcd.print("Liquid1 cm:");
+    lcd.setCursor(13,1);
+    lcd.print(x);
 }
 
 void info(int height, int offset)
@@ -142,13 +268,15 @@ void info(int height, int offset)
     lcd.clear();
     lcd.print("Height:");
     lcd.setCursor(9,0);
-    lcd.print(height);
+    lcd.print(height - offset);
     lcd.setCursor(9+sizeof(height),0);
     lcd.print("cm");
     lcd.setCursor(0,1);
     lcd.print("Offset:");
     lcd.setCursor(9,1);
     lcd.print(offset);
-    lcd.setCursor(9+sizeof(offset),1);
+    lcd.setCursor(9,1);
+    lcd.print("+");
+    lcd.setCursor(10+sizeof(offset),1);
     lcd.print("cm");
 }
