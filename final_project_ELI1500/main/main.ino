a199dee2cada91e6c113e4bee429a99efffb2d92
secretmud <lamefeed@gmail.com>
2019-04-28 23:25:08 +0200

    Fixed a type in function prototypes

--- final_project_ELI1500/main/main.ino
+++ final_project_ELI1500/main/main.ino
@@ -4,10 +4,8 @@
 #define menu_len 16
 #define full_glass 12000
 //Pin declaration
-
 int trigger[] = {5, 7, 9};
 int echo[] = {6, 8, 10};
-
 int btn = 2;
 int dt = 3;
 int clk = 4;
@@ -15,18 +13,13 @@ int motor1 = 11;
 int motor2 = 12;
 int led = 13;
 
-bool set = false;
-bool rand_set = false;
-
-double height;
-double box1;
-double box2;
-double glass;
-
 //Used by the ultrasonic sensor
 bool calibrated = false;
 long distance;
 long duration;
+double height;
+double box1;
+double box2;
 
 //Rotatry encoder
 int counter = 0;
@@ -43,14 +36,14 @@ int in [] = {3, 4, 6,
              8, 10};
 
 //function prototypes
+
 void calibration_print(double height, double tank1, double tank2);
 void game_print(double height, double dist);
 void menu_fields(int x);
-void menu_switch(int counter, double height, float offset, double box1, double box2);
-void programs(float height, bool set, float percentage, int offset);
+void menu_switch(int counter, double height, double box1, double box2);
+void programs(double height, bool set, bool rand_set, int percentage);
 void lcd_line_clear(int start, int collumn, int rows);
 void tank_low(int tank);
-
 //Initializing the 16x2 LCD with a I2C connector
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -81,7 +74,7 @@ void loop() {
         height = front.measureDistanceCm();
         box1 = tank1.measureDistanceCm();
         box2 = tank2.measureDistanceCm();
-        calibration_print(height - glass, box1, box2);
+        calibration_print(height, box1, box2);
         calibrated = true;
     } else {
         //Checking the states and appending a 1 or subtracting a one to counter depending on the placement of the rotary encoder
@@ -102,14 +95,14 @@ void loop() {
 
         //Checking for "LOW" since pinMode for btn is INPUT_PULLUP
         if (digitalRead(btn) == LOW) {
-            menu_switch(counter, height, glass, box1, box2);
+            menu_switch(counter, height, box1, box2);
         }
 
         dtLastState = dtState;
     }
 }
 
-void menu_switch(int counter, double height, double glass, double box1, double box2) {
+void menu_switch(int counter, double height, double box1, double box2) {
     //Checking the distance to the water in the two water tanks
     if (tank1.measureDistanceCm() > 22) {
         tank_low(0);
@@ -130,23 +123,19 @@ void menu_switch(int counter, double height, double glass, double box1, double b
             }
             break;
         case 1:
-            if (tank1.measureDistanceCm() >= 25) {
-                digitalWrite(led, HIGH);
-                break;
-            }
-            programs(height, true, false, 10, glass);
+            programs(height, true, false, 10);
             break;
         case 2:
-            programs(height, true, false, 20, glass);
+            programs(height, true, false, 20);
             break;
         case 3:
-            programs(height, true, false, 30, glass);
+            programs(height, true, false, 30);
             break;
         case 4:
-            programs(height, false, true, 0, glass);
+            programs(height, false, true, 0);
             break;
         case 5:
-            programs(height, false, false, 0, glass);
+            programs(height, false, false, 0);
             break;
         case 6:
             calibration_print(height, box1, box2);
@@ -167,7 +156,7 @@ void menu_fields(int counter) {
     lcd.print(menu[counter + 1]);
 }
 
-void programs(double height, bool set, bool rand_set, double percentage, double glass) {
+void programs(double height, bool set, bool rand_set, int percentage) {
     //This is where all the program logic is placed, we're using booleans or the lack thereof to choose which program to run.
     double dist;
     double ho;
@@ -236,11 +225,11 @@ void game_print(double height, double x) {
     lcd.print("\%");
 }
 
-void calibration_print(double height, double tank1, double tank2) {
+void calibration_print(double height, double box1, double box2) {
     double arr[] = {
         height,
-        tank1,
-        tank2
+        box1,
+        box2
     };
     char name[][6] = {{"Glass"}, {"Tank1"}, {"Tank2"}};
     lcd.clear();
